bc4ad30c504431764a0e6763e81804d6afa58eb7
blasrodriguez <blasrodriguez934@gmail.com>
2020-04-07 12:25:16 -0300

    se agrega la parte de editar un articulo

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Crear Articulo</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form enctype="multipart/form-data" action="{{ route('posts.store')}}" method="POST">
+                    
+                        <div class="form-group">
+                            <label >Titulo *</label>
+                            <input type="text" name="title" class="form-control" requered>
+                        </div>
+                        <div class="form-group">
+                            <label >Imagen</label>
+                            <input type="file" name="file" >
+                        </div>
+                        <div class="form-group">
+                            <label >Contenido *</label>
+                            <textarea name="body"  rows="6" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label >Contenido embebido</label>
+                            <textarea name="iframe"  requered class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            @csrf
+                            <input type="sumbit" value="Enviar" class="btn btn-sm btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
